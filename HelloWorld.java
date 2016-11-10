5f7fe799689d348c11ef37dba9207d3a33085c7f
mrithore <mritunjoy_hore@rediffmail.com>
2016-11-10 12:39:28 +0530

    Changes made

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 	public static void main (String[] args){
-		for (int i=1; i<=5; i++){		
+		for (int i=1; i<=4; i++){		
 		System.out.println("Continuous Integration using Jenkins "+ i);
 		}
 	}
